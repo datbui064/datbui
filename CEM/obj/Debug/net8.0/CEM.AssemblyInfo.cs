626ac332038dff7f3f65ec8607f94d575273a857
TruongHan <nguyentruonghanbl03@gmail.com>
2024-11-24 13:03:56 +0700

    dsds

--- CEM/obj/Debug/net8.0/CEM.AssemblyInfo.cs
+++ CEM/obj/Debug/net8.0/CEM.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CEM")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c247187605f62ee095ad2b2f60d7075ffe2e00a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e6ffa14f78348e22e200b856db44922a96e4e891")]
 [assembly: System.Reflection.AssemblyProductAttribute("CEM")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CEM")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
